ddd1a06cda7b8fc0a491783fd7c020845f2dfb19
2otogizoshi <47193542+2otogizoshi@users.noreply.github.com>
2019-02-09 21:04:05 -0600

    State change
    changed friends array to work as a component with keyword this in constructor

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,19 @@ import FriendsList from "./FriendsList";
 //a
 
 class App extends React.Component {
-  render() {
-    const friends = ["Jordyn", "Mikenzi", "Jake"];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      friends: ["Jordyn", "Mikenzi", "Jake"]
+    };
+  }
+  render() {
     return (
       <div>
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
-        <FriendsList list={friends} />
+        <FriendsList list={this.state.friends} />
       </div>
     );
   }
