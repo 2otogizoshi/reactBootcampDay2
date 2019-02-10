6df2c435459d2bbc439caaeb988c851ca7b4bab8
2otogizoshi <47193542+2otogizoshi@users.noreply.github.com>
2019-02-09 20:01:53 -0600

    changed App function to App class

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import FriendsList from "./FriendsList";
 //a
-function App() {
-  const friends = ["Jordyn", "Mikenzi", "Jake"];
-  return (
-    <div>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <FriendsList list={friends} />
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    const friends = ["Jordyn", "Mikenzi", "Jake"];
+
+    return (
+      <div>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <FriendsList list={friends} />
+      </div>
+    );
+  }
 }
 
 export default App;
