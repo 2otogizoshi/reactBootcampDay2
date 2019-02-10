c6d933ea9c99fff074b49beac8071530eb25061a
2otogizoshi <47193542+2otogizoshi@users.noreply.github.com>
2019-02-09 19:49:08 -0600

    Friendslist Passed Values
    passed value friends array

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import FriendsList from "./FriendsList";
 //a
 function App() {
+  const friends = ["Jordyn", "Mikenzi", "Jake"];
   return (
     <div>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <FriendsList />
+      <FriendsList list={friends} />
     </div>
   );
 }
